d9420329e5f8af3ac9aca7aa7e0e8629e910a533
ElliottCY <elliott.curtis.young@gmail.com>
2026-02-01 19:10:15 -0500

    Added menu bar

---
+++ qset-website/src/Menu_bar.js
@@ -0,0 +1,18 @@
+import './Menu_bar.css';
+
+const Menu_bar = () => {
+    return ( 
+        <div className="menu-bar">
+            <img src="/qset_logo_image.png" alt="QSET Logo" className="menu-logo" />
+            <div className="menu-items">
+                <div className="menu-item">Rover</div>
+                <div className="menu-item">Satellite</div>
+                <div className="menu-item">Exec</div>
+                <div className="menu-item">Sponsors</div>
+            </div>
+        </div>
+
+     );
+}
+ 
+export default Menu_bar;
